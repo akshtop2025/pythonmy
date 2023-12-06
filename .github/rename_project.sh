8a104df1b6bf53c8efa7aa788b72baac19a12ec1
akshtop2025 <akshtop2025@users.noreply.github.com>
2023-12-06 10:32:17 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="akshtop2025"
+original_name="pythonmy"
+original_urlname="pythonmy"
+original_description="Awesome pythonmy created by akshtop2025"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pythonmy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ pythonmy/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for pythonmy."""
+
+from pythonmy.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
